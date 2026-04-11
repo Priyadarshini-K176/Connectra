37b146d8ed409349be8bf31ea699bb1508ad16fb
Priyadarshini-K176 <priyadarshini232005@gmail.com>
2026-04-11 21:36:30 +0530

    Payment fixed

--- backend/src/routes/profile.js
+++ backend/src/routes/profile.js
@@ -54,6 +54,7 @@ profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
     res.status(200).json({
       success: true,
       message: `${loggedInUser.firstName}, your profile has been updated`,
+      user: loggedInUser
     });
 
   } catch (err) {
@@ -241,18 +242,16 @@ profileRouter.patch(
         : [];
       if (adminEmails.includes(user.email)) {
         return res.status(400).json({
-          error: `Role of ${
-            user.firstName[0].toUpperCase() + user.firstName.slice(1)
-          } could not be update.`,
+          error: `Role of ${user.firstName[0].toUpperCase() + user.firstName.slice(1)
+            } could not be update.`,
         });
       }
 
       //* if role is same as previous role
       if (user.role === role) {
         return res.status(400).json({
-          error: `${
-            user.firstName[0].toUpperCase() + user.firstName.slice(1)
-          } already have ${role} role`,
+          error: `${user.firstName[0].toUpperCase() + user.firstName.slice(1)
+            } already have ${role} role`,
         });
       }
 
@@ -261,9 +260,8 @@ profileRouter.patch(
       await user.save();
 
       res.status(200).json({
-        message: `Role of ${
-          user.firstName[0].toUpperCase() + user.firstName.slice(1)
-        } is updated to ${role}`,
+        message: `Role of ${user.firstName[0].toUpperCase() + user.firstName.slice(1)
+          } is updated to ${role}`,
       });
     } catch (err) {
       console.log(err);
@@ -299,8 +297,8 @@ profileRouter.get(
         parseInt(req.query.limit) > 50
           ? 50
           : parseInt(req.query.limit) < 1
-          ? 1
-          : parseInt(req.query.limit) || 10;
+            ? 1
+            : parseInt(req.query.limit) || 10;
 
       const list = await User.find({ role: role })
         .select(process.env.ALLOWED_FIELDS.split(","))
@@ -503,9 +501,8 @@ profileRouter.patch(
         : [];
       if (adminEmails.includes(user.email)) {
         return res.status(400).json({
-          error: `Role of ${
-            user.firstName[0].toUpperCase() + user.firstName.slice(1)
-          } could not be update.`,
+          error: `Role of ${user.firstName[0].toUpperCase() + user.firstName.slice(1)
+            } could not be update.`,
         });
       }
 
@@ -518,9 +515,8 @@ profileRouter.patch(
       //* checking if new role is same as previous
       if (user.role === role) {
         return res.status(400).json({
-          message: `${
-            user.firstName[0].toUpperCase() + user.firstName.slice(1)
-          } already have ${role} role`,
+          message: `${user.firstName[0].toUpperCase() + user.firstName.slice(1)
+            } already have ${role} role`,
         });
       }
 
@@ -529,9 +525,8 @@ profileRouter.patch(
       await user.save();
 
       res.status(200).json({
-        message: `Role of ${
-          user.firstName[0].toUpperCase() + user.firstName.slice(1)
-        } is updated to ${role}`,
+        message: `Role of ${user.firstName[0].toUpperCase() + user.firstName.slice(1)
+          } is updated to ${role}`,
       });
     } catch (err) {
       console.log(err);
@@ -570,8 +565,8 @@ profileRouter.get(
         parseInt(req.query.limit) > 50
           ? 50
           : parseInt(req.query.limit) < 1
-          ? 1
-          : parseInt(req.query.limit) || 10;
+            ? 1
+            : parseInt(req.query.limit) || 10;
 
       const list = await User.find({ role: role })
         .select(process.env.ALLOWED_FIELDS.split(","))
@@ -659,8 +654,8 @@ profileRouter.get(
         parseInt(req.query.limit) > 50
           ? 50
           : parseInt(req.query.limit) < 1
-          ? 1
-          : parseInt(req.query.limit) || 10;
+            ? 1
+            : parseInt(req.query.limit) || 10;
 
       // Determine sorting order (default to descending by createdAt)
       const sortBy = req.query.sortBy || "createdAt";
