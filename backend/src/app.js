5286b417a0d9cb4a1f1c6ab85eb7c288a297cebd
Priyadarshini-K176 <priyadarshini232005@gmail.com>
2026-04-11 21:56:57 +0530

    Auth fixed for render

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,7 +9,7 @@ const app = express();
 // ✅ Middleware
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174", "https://connectra-rfzppk0f2-priyas-projects-dd82cdd4.vercel.app"],
+    origin: ["http://localhost:5173", "http://localhost:5174", "https://connectra-rose.vercel.app/"],
     credentials: true,
   })
 );
